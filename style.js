9b512bdc9cabfe41e95aa2e21d425e9c37ba6555
Luciano Cuaranta <lucianocuaranta@gmail.com>
2023-08-31 18:36:40 -0300

    json

--- style.js
+++ style.js
@@ -1,5 +1,5 @@
 class Producto {
-    constructor({id, nombre, precio, descripcion, img}) {
+    constructor({ id, nombre, precio, descripcion, img }) {
         this.id = id
         this.nombre = nombre
         this.precio = precio
@@ -8,12 +8,12 @@ class Producto {
         this.img = img
     }
 
-    aumentarCantidad(){
+    aumentarCantidad() {
         this.cantidad++
     }
 
-    disminuirCantidad(){
-        if(this.cantidad > 1){
+    disminuirCantidad() {
+        if (this.cantidad > 1) {
             this.cantidad--
             return true
         }
@@ -21,43 +21,11 @@ class Producto {
         return false
     }
 
-    descripcionCarrito(){
-        return `
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${this.img}" class="img-fluid rounded-start class="chau"" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">${this.nombre}</h5>
-                        <p class="card-text">Cantidad: <button id="minus-${this.id}">-</button> ${this.cantidad} <button id="plus-${this.id}">+</button></p>
-                        <p class="card-text">$${this.precio}</p>
-                        <p class="card-text"><small class="text-body-secondary">${this.descripcion}</small></p>
-                        <button id="eliminar-${this.id}"><i class="fa-solid fa-trash" style="color: #0f0f0f;"></i></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
-    }
-
-    descripcionHTML(){
-        return `<div class="div-uno">
-            <img src="${this.img}">
-            <div class="card-uno">
-                <h2>${this.nombre}</h2>
-                <p>$${this.precio}</p>
-                <p>${this.descripcion}</p>
-                <a href="" id="pro${this.id}">Buy Now</a>
-            </div>
-            </div>
-            `
-    }
+    
 }
 
-class Carrito{
-    constructor(){
+class Carrito {
+    constructor() {
         this.listacarrito = []
         this.contenedor_carrito = document.getElementById('contenedor_carrito')
         this.total = document.getElementById('total')
@@ -65,47 +33,47 @@ class Carrito{
         this.llaveStorage = "listacarrito"
     }
 
-    levantarStorage(){
+    levantarStorage() {
         this.listacarrito = JSON.parse(localStorage.getItem(this.llaveStorage)) || []
-        
-        if(this.listacarrito.length > 0){
+
+        if (this.listacarrito.length > 0) {
             let listaAuxiliar = []
 
             for (let i = 0; i < this.listacarrito.length; i++) {
-                
+
                 let productoBis = new Producto(this.listacarrito[i])
                 listaAuxiliar.push(productoBis)
-                
-                
+
+
             }
 
             this.listacarrito = listaAuxiliar
         }
     }
-    guardarStorage(){
+    guardarStorage() {
         let listaCarritoJSON = JSON.stringify(this.listacarrito)
         localStorage.setItem(this.llaveStorage, listaCarritoJSON)
     }
-    
+
     agregar(productoNuevo) {
         let existeElProducto = this.listacarrito.some(producto => producto.id == productoNuevo.id)
-        
-        if( existeElProducto ){
+
+        if (existeElProducto) {
             let producto = this.listacarrito.find(producto => producto.id == productoNuevo.id)
             producto.cantidad = producto.cantidad + 1
-        }else{
-            this.listacarrito.push(productoNuevo)  
+        } else {
+            this.listacarrito.push(productoNuevo)
         }
     }
 
-    eliminar(productoEliminado){
+    eliminar(productoEliminado) {
         let producto = this.listacarrito.find(producto => producto.id == productoEliminado.id)
         let indice = this.listacarrito.indexOf(producto)
-        this.listacarrito.splice(indice,1)
-        
+        this.listacarrito.splice(indice, 1)
+
     }
 
-    limpiarContenedorCarrito(){
+    limpiarContenedorCarrito() {
         this.contenedor_carrito.innerHTML = ""
     }
 
@@ -113,7 +81,7 @@ class Carrito{
 
         this.contenedor_carrito.innerHTML = ""
 
-        this.listacarrito.forEach( producto => {
+        this.listacarrito.forEach(producto => {
             contenedor_carrito.innerHTML += producto.descripcionCarrito()
         })
 
@@ -130,13 +98,13 @@ class Carrito{
                 this.ver()
             })
 
-            btn_plus.addEventListener("click",()=>{
+            btn_plus.addEventListener("click", () => {
                 producto.aumentarCantidad()
                 this.ver()
             })
 
-            btn_minus.addEventListener("click", ()=>{
-                if( producto.disminuirCantidad() ){
+            btn_minus.addEventListener("click", () => {
+                if (producto.disminuirCantidad()) {
                     this.ver()
                 }
             })
@@ -146,22 +114,22 @@ class Carrito{
 
     }
 
-    
 
-    calcular_total(){
-        return this.listacarrito.reduce((acumulador, producto) => acumulador + producto.precio * producto.cantidad ,0)
+
+    calcular_total() {
+        return this.listacarrito.reduce((acumulador, producto) => acumulador + producto.precio * producto.cantidad, 0)
     }
 
-    eventoFinalizarCompra(){
+    eventoFinalizarCompra() {
         this.finalizar_compra.addEventListener("click", () => {
 
-            if(this.listacarrito.length > 0){
+            if (this.listacarrito.length > 0) {
                 let precio_total = this.calcular_total()
-                
+
                 this.listacarrito = []
-                
+
                 localStorage.removeItem(this.llaveStorage)
-                
+
                 this.limpiarContenedorCarrito()
 
                 let mensaje = ""
@@ -177,10 +145,10 @@ class Carrito{
                 } else {
                     mensaje = "Muchas gracias por visitar nuestra página!";
                 }
-                
-                this.contenedor_carrito.innerHTML = mensaje  
-                this.total.innerText = 'Precio final de su compra actual: $'+ precio_total
-            }else{
+
+                this.contenedor_carrito.innerHTML = mensaje
+                this.total.innerText = 'Precio final de su compra actual: $' + precio_total
+            } else {
                 this.contenedor_carrito.innerHTML = '<h3 class="text-center">Gracias por visitar nuestra pagina!</h3>'
             }
 
@@ -211,7 +179,15 @@ class Productocontrolador {
                 carrito.agregar(producto)
                 carrito.guardarStorage()
                 carrito.ver()
-
+                Toastify({
+                    text: "Su producto fue añadido al carrito",
+                    duration: 2000,
+                    gravity: "bottom", 
+                    position: "right", 
+                    style: {
+                        background: "#855235",
+                    },
+                }).showToast();
             }
             )
         })
@@ -221,11 +197,30 @@ class Productocontrolador {
 
 
 
-const productoUno = new Producto({id:1, nombre:"Hoodie Classic", precio:13000, descripcion: "Buzo negro Oversize Classic", img:"./img/clothes-chica-redu.jpg" })
 
-const productoDos = new Producto({id:2, nombre:"Hoodie System", precio:16000, descripcion:"Buzo negro Oversize System", img:"./img/clothes-escalera-redu.jpg"})
+fetch("archivo.json")
+.then(resp => resp.json())
+.then(listaproductos => {
+    render(listaproductos)
+})
 
-const productoTres = new Producto({id:3, nombre:"Remera Classic", precio:10000, descripcion:"Remera negra Oversize Classic", img:"./img/clothes-reme-classic-redu.jpg"})
+
+function render(arr){
+    let mis_productos = document.getElementById("mis_productos")
+    arr.forEach(producto => {
+        mis_productos.innerHTML+= `<div class="div-uno" style="width: 16rem;" gap-5>
+        <img src="${producto.img}" class="card-img-top" >
+        <div class="card-uno">
+            <h2>${producto.nombre}</h2>
+            <p>$${producto.precio}</p>
+            <p>${producto.descripcion}</p>
+            <button id="pro${producto.id}">Buy Now</button>
+        </div>
+        </div>
+        ` 
+    })
+}
+
 
 const carrito = new Carrito()
 carrito.levantarStorage()
@@ -236,7 +231,5 @@ carrito.eventoFinalizarCompra()
 
 const controlador_productos = new Productocontrolador()
 
-controlador_productos.agregar(productoUno)
-controlador_productos.agregar(productoDos)
-controlador_productos.agregar(productoTres)
+
 controlador_productos.verProductos()
